8219d6d6d3bbed1cc060ca1e84d0619ef082854f
niwwka <senya123op@yandex.ru>
2025-12-23 15:35:19 +0300

    angular drag + enemy linear drag

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -5,26 +5,28 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 
 {
+    private Rigidbody2D rb2D;
     public GameObject player;
     public float speed;
+    public Vector2 direction;
+    public float force;
 
-    private float distance;
+    //private float distance;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
+        //distance = Vector2.Distance(transform.position, player.transform.position);
+        direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        //transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        rb2D.AddForce(direction * force, ForceMode2D.Force);
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,27 +6,33 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Settings")]
     //[SerializeField] private float acc = 1f;
     [SerializeField] private float f1 = 10.0f;
-    [SerializeField] private float mass = 10.0f;
-    [SerializeField] private float t = 0f;
     [SerializeField] private float nu = 0.001f;
-    [SerializeField] private Vector2 acc = Vector2.zero;
-    [SerializeField] private Vector2 v = Vector2.zero; 
+    [SerializeField] private Vector2 direction = Vector2.zero;
+    private Rigidbody2D rb2D;
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        direction = new Vector2(horizontal, vertical).normalized;
 
-        Vector3 direction = new Vector2(horizontal, vertical).normalized;
         Vector2 f1_vec = direction * f1;
-        Vector2 f2_vec = -v.normalized * mass * nu;
+        Vector2 f2_vec = -rb2D.velocity.normalized * rb2D.mass * nu;
 
-        Vector2 f_sum = f1_vec + f2_vec;
-        acc = f_sum / mass;
-        v += acc;
+        rb2D.AddForce(f1_vec, ForceMode2D.Force);
+        //rb2D.AddForce(f2_vec, ForceMode2D.Force);
 
-        Vector3 v3d = new Vector3(v.x, v.y, 0);
-        transform.position += v3d;
+        //Vector2 f_sum = f1_vec + f2_vec;
+        //acc = f_sum / mass;
+        //v += acc;
+
+        //Vector3 v3d = new Vector3(v.x, v.y, 0);
+        //transform.position += v3d;
     }
 }
